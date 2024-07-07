064d672505fd0675b55d2541accc99583a9285d2
helal0321 <159494281+helal0321@users.noreply.github.com>
2024-07-07 23:22:48 +0300

    Update paypal.config.js

--- server/config/paypal.config.js
+++ server/config/paypal.config.js
@@ -1,7 +1,7 @@
 const paypal=require("paypal-rest-sdk")
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': 'AX5d1pg4NanHkkSTyCOnQ7dI1gbBx4lSsXsLKivEgVdXVgDSHgMFUix2bZzHjKS6HdeXQYZ8dQoq_ytx',
-    'client_secret': 'ECzsljw4zaHEeqDlkLZvE3lA7TwY9tMh0htxy4TtKPNR-iJUE6mFQCb6NqggFgDHjb9gH4sRK4gv3Ife'
+    'client_id': process.env.PAYPAL_CLIENT_ID,
+    'client_secret': process.env.PAYPAL_CLIENT_SECRET
   });
   module.exports=paypal
